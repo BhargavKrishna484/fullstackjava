57de58b659bdd76be613470f4bf24cc3c1caf5bc
BhargavKrishna484 <vaddadibhargavkrishna@gmail.com>
2025-06-18 15:49:18 +0530

    Add files via upload

---
+++ Operator/Operator1.java
@@ -0,0 +1,53 @@
+public class Operator1{
+    public static void main(String[] args) {
+          int a = 10, b = 5;
+ boolean x = true, y = false;
+       /* System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));  
+        System.out.println("a - b = " + (a - b));  
+        System.out.println("a * b = " + (a * b));  
+        System.out.println("a / b = " + (a / b));  
+        System.out.println("a % b = " + (a % b));  
+Output:Arithmetic Operators:
+a + b = 15
+a - b = 5
+a * b = 50
+a / b = 2
+a % b = 0
+ System.out.println("\nIncrement/Decrement Operators:");
+ System.out.println("a++: " + (a++)); 
+ System.out.println("++a: " + (++a));
+ System.out.println("a--: " + (a--)); 
+ System.out.println("--a: " + (--a)); 
+output:Increment/Decrement Operators:
+a++: 10
+++a: 12
+a--: 12
+--a: 10
+ System.out.println("\nRelational Operators:");
+ System.out.println("a == b: " + (a == b)); 
+ System.out.println("a != b: " + (a != b)); 
+System.out.println("a > b: " + (a > b));    
+        System.out.println("a < b: " + (a < b));   
+        System.out.println("a >= b: " + (a >= b));  
+        System.out.println("a <= b: " + (a <= b));
+        
+
+Output:Relational Operators:
+a == b: false
+a != b: true
+  System.out.println("Logical Operators:");
+        System.out.println("x && y: " + (x && y));  
+        System.out.println("x || y: " + (x || y));  
+        System.out.println("!x: " + (!x)); 
+Output:Logical Operators:
+x && y: false
+x || y: true
+!x: false 
+a > b: true
+a < b: false
+a >= b: true
+a <= b: false*/
+
+}
+}
